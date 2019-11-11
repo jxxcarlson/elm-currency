f444c4e0908f139bfe52bacc237937b3b1e8a3a9
James Carlson <jxxcarlson@mac.com>
2019-11-11 15:28:08 +0100

    Add: functions to reconcile account

--- src/Currency.elm
+++ src/Currency.elm
@@ -1,4 +1,4 @@
-module Currency exposing(..)
+module Currency exposing(Money)
 
 {-| The currency module models currency with an identity
 and an expiration.
@@ -7,6 +7,7 @@ and an expiration.
 
 import List.Extra
 import String.Interpolate exposing(interpolate)
+import Maybe.Extra
 
 
 {-| Currency is the fundamental type of this module.
@@ -37,6 +38,127 @@ issuedAt (Money m) = m.issuedAt
 
 expiresAt : Money -> Expiration
 expiresAt (Money m) = m.expiresAt
+
+
+sameIssue : Money -> Money -> Bool
+sameIssue a b =
+    issuedAt a == issuedAt b
+
+sameExpiration : Money -> Money -> Bool
+sameExpiration a b =
+    expiresAt a == expiresAt b
+
+
+{-|
+
+    c1 : Money
+    c1 =  Money {amount = Cents 123, currency = Complementary Cambiatus, issuedAt = BankTime 0, expiresAt = Finite (BankTime 100) }
+
+    c2 : Money
+    c2 =  Money {amount = Cents 123, currency = Complementary Cambiatus, issuedAt = BankTime 10, expiresAt = Finite (BankTime 100) }
+
+    c3 : Money
+    c3 =  Money {amount = Cents 123, currency = Complementary Cambiatus, issuedAt = BankTime 0, expiresAt = Finite (BankTime 200) }
+
+    samePeriod c1 c1
+    --> True
+
+    samePeriod c1 c2
+    --> False
+
+    samePeriod c1 c3
+    --> False
+-}
+samePeriod : Money -> Money -> Bool
+samePeriod a b =
+    issuedAt a == issuedAt b && expiresAt a == expiresAt b
+
+{-|
+    c1 : Money
+    c1 =  Money {amount = Cents 100, currency = Complementary Cambiatus, issuedAt = BankTime 0, expiresAt = Finite (BankTime 100) }
+
+    c2 : Money
+    c2 =  Money {amount = Cents 200, currency = Complementary Cambiatus, issuedAt = BankTime 0, expiresAt = Finite (BankTime 100) }
+
+    c3 : Money
+    c3 =  Money {amount = Cents 300, currency = Complementary Cambiatus, issuedAt = BankTime 10, expiresAt = Finite (BankTime 100) }
+
+    group [c1]
+    --> [[c1]]
+
+    group []
+    --> []
+
+    group [c1, c2, c3]
+    --> [[c1,c2], [c3]]
+-}
+group : List Money -> List (List Money)
+group list =
+    list
+      |> List.Extra.groupWhile samePeriod
+      |> List.map (\item -> (Tuple.first item)::(Tuple.second item))
+
+
+
+{-| This is DANGEROUS function.  It assumes
+that all elements of the list have the
+same period ahd are of the same currency.
+It should only be called by functions that
+establish these conditions.
+
+    c1 : Money
+    c1 =  Money {amount = Cents 100, currency = Complementary Cambiatus, issuedAt = BankTime 0, expiresAt = Finite (BankTime 100) }
+
+    c2 : Money
+    c2 =  Money {amount = Cents 200, currency = Complementary Cambiatus, issuedAt = BankTime 0, expiresAt = Finite (BankTime 100) }
+
+    c3 : Money
+    c3 =  Money {amount = Cents 300, currency = Complementary Cambiatus, issuedAt = BankTime 0, expiresAt = Finite (BankTime 100) }
+
+    consolidate [c1,c2]
+    --> Just c3
+-}
+consolidate : List Money -> Maybe Money
+consolidate list =
+    case List.head list of
+        Nothing -> Nothing
+        Just (Money m) ->
+           Just <| Money { m | amount = valueInCents_ list}
+
+
+{-|
+
+    c1 : Money
+    c1 =  Money {amount = Cents 100, currency = Complementary Cambiatus, issuedAt = BankTime 0, expiresAt = Finite (BankTime 100) }
+
+    c2 : Money
+    c2 =  Money {amount = Cents 200, currency = Complementary Cambiatus, issuedAt = BankTime 0, expiresAt = Finite (BankTime 100) }
+
+    c3 : Money
+    c3 =  Money {amount = Cents 300, currency = Complementary Cambiatus, issuedAt = BankTime 0, expiresAt = Finite (BankTime 100) }
+
+
+    c4 : Money
+    c4 =  Money {amount = Cents 10, currency = Complementary Cambiatus, issuedAt = BankTime 0, expiresAt = Infinite }
+
+    acct : Account
+    acct = Account {currency = Complementary Cambiatus, transactions = [c1,c2]}
+
+    simplify (BankTime 0) acct
+    --> Account {currency = Complementary Cambiatus, transactions = [c3]}
+
+    simplify (BankTime 0) (Account {currency = Complementary Cambiatus, transactions = [c1,c2, c4]})
+    --> Account {currency = Complementary Cambiatus, transactions = [c3, c4]}
+-}
+simplify : BankTime -> Account -> Account
+simplify bt ((Account acct) as account) =
+   let
+       (Account acct2) = ensureValid bt account
+       groups = group acct2.transactions
+       transactions = List.map consolidate groups
+         |> Maybe.Extra.values
+   in
+     Account {currency = acct.currency, transactions = transactions}
 
 
 type Value = Value Currency Cents
@@ -187,10 +309,23 @@ value  bankTime ((Account acct) as account)=
 -}
 valueInCents : Account -> Cents
 valueInCents (Account acct) =
-  case acct.transactions of
+    valueInCents_ acct.transactions
+--  case acct.transactions of
+--      [] -> (Cents 0)
+--      transactions ->
+--         transactions
+--            |> List.map amount
+--            |> List.map (\(Cents k) -> k)
+--            |> List.sum
+--            |> (\s -> Cents s)
+
+
+valueInCents_ : List Money -> Cents
+valueInCents_ list  =
+  case list of
       [] -> (Cents 0)
-      transactions ->
-         transactions
+      _ ->
+         list
             |> List.map amount
             |> List.map (\(Cents k) -> k)
             |> List.sum
